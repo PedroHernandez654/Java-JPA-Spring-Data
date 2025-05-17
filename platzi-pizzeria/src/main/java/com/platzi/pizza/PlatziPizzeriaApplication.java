a533d54ae4c9621bd03f0a952c379ad13cbc549f
PedroHernandez654 <hernandezdzulpedro2@gmail.com>
2025-05-17 11:32:30 -0600

    Crud Repository
    Se agregó el repositorio tipo CRUD para hacer las acciones

--- platzi-pizzeria/src/main/java/com/platzi/pizza/PlatziPizzeriaApplication.java
+++ platzi-pizzeria/src/main/java/com/platzi/pizza/PlatziPizzeriaApplication.java
@@ -2,8 +2,11 @@ package com.platzi.pizza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//Para utilizar el repositorio de JPA se requiere este @
+@EnableJpaRepositories
 public class PlatziPizzeriaApplication {
 
 	public static void main(String[] args) {
--- platzi-pizzeria/src/main/java/com/platzi/pizza/service/PizzaService.java
+++ platzi-pizzeria/src/main/java/com/platzi/pizza/service/PizzaService.java
@@ -1,6 +1,7 @@
 package com.platzi.pizza.service;
 
 import com.platzi.pizza.persistence.entity.PizzaEntity;
+import com.platzi.pizza.persistence.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,18 +11,19 @@ import java.util.List;
 
 @Service
 public class PizzaService {
-    //Consultas en nuestro servicio
-    private final JdbcTemplate jdbcTemplate;
+    private final PizzaRepository pizzaRepository;
 
     //Inyección de dependencias
     @Autowired
-    public PizzaService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public PizzaService(PizzaRepository pizzaRepository) {
+        this.pizzaRepository = pizzaRepository;
     }
 
     public List<PizzaEntity>getAll(){
         //Esto nos permite hacer consultas de SQL y convertirlas a clases JAVA
-        return this.jdbcTemplate.query("SELECT * FROM pizza WHERE available = 0", new BeanPropertyRowMapper<>(PizzaEntity.class));
+        //Esto es JDBC
+        //return this.jdbcTemplate.query("SELECT * FROM pizza WHERE available = 0", new BeanPropertyRowMapper<>(PizzaEntity.class));
+        return this.jdbcTemplate.query("SELECT * FROM pizza", new BeanPropertyRowMapper<>(PizzaEntity.class));
     }
 
 }
