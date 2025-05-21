570eaa066b6df1369a112d55f8a9695b439c66b2
PedroHernandez654 <hernandezdzulpedro2@gmail.com>
2025-05-21 17:13:26 -0600

    Uso de los projections
    Se hizo una interfaz como tipo DTO la cual solo muestra los datos necesarios, realizamos la busqueda con SQL nativo y les pusimos sus atributos nombrandolos de la misma manera

--- platzi-pizzeria/src/main/java/com/platzi/pizza/persistence/repository/OrderRepository.java
+++ platzi-pizzeria/src/main/java/com/platzi/pizza/persistence/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.platzi.pizza.persistence.repository;
 
 import com.platzi.pizza.persistence.entity.OrderEntity;
+import com.platzi.pizza.persistence.projection.OrderSummary;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +17,16 @@ public interface OrderRepository extends ListCrudRepository<OrderEntity, Integer
     //Se le agrega el nativeQuery por que si no lo interpetra como JPQL
     @Query(value = "SELECT * FROM pizza_order WHERE id_customer = :id", nativeQuery = true)
     List<OrderEntity> findCustomerOrders(@Param("id") String idCustomer);
+
+    //Tener cuidado con los espacios en la consulta, por que pueden haber errores de syntax
+    @Query(value =
+            "SELECT po.id_order AS idOrder, cu.name AS customerName, po.date AS orderDate," +
+            "   po.total AS orderTotal, group_concat(pi.name) AS pizzaName " +
+            "FROM pizza_order po " +
+            "   INNER JOIN customer cu ON po.id_customer = cu.id_customer " +
+            "   INNER JOIN order_pizza oi ON po.id_order = oi.id_order " +
+            "   INNER JOIN pizza pi on oi.id_pizza = pi.id_pizza " +
+            "WHERE po.id_order = :orderId " +
+            "group by po.id_order, cu.name, po.date, po.total", nativeQuery = true)
+    OrderSummary findSummary(@Param("orderId")int orderId);
 }
